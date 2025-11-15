ff8bfdeea5fc4b5891344c66ef16da84d6ca5b14
Tanjilur Rahman Jim <tanjilurrahman21@gmail.com>
2025-11-15 12:03:41 +0600

    changed student controller to use the public path for saving image

--- app/Http/Controllers/Api/StudentController.php
+++ app/Http/Controllers/Api/StudentController.php
@@ -7,27 +7,34 @@ use App\Http\Requests\StudentRequest;
 use App\Http\Resources\StudentResource;
 use App\Models\Student;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
 
 class StudentController extends Controller
 {
+
     public function index(Request $req)
     {
         $q = Student::query();
+
         if ($req->filled('class')) $q->where('class', $req->class);
         if ($req->filled('search')) $q->where('name', 'like', '%' . $req->search . '%');
+
         $students = $q->orderBy('name')->paginate(12);
+
         return StudentResource::collection($students);
     }
 
     public function store(StudentRequest $req)
     {
         $data = $req->validated();
+
         if ($req->hasFile('photo')) {
-            $path = $req->file('photo')->store('students', 'public');
-            $data['photo'] = $path;
+            $filename = time() . '_' . $req->photo->getClientOriginalName();
+            $req->photo->move(public_path('students'), $filename);
+            $data['photo'] = $filename;
         }
+
         $student = Student::create($data);
+
         return (new StudentResource($student))->response()->setStatusCode(201);
     }
 
@@ -39,18 +46,35 @@ class StudentController extends Controller
     public function update(StudentRequest $req, Student $student)
     {
         $data = $req->validated();
+
         if ($req->hasFile('photo')) {
-            if ($student->photo) Storage::disk('public')->delete($student->photo);
-            $data['photo'] = $req->file('photo')->store('students', 'public');
+
+            // Delete old image
+            if ($student->photo) {
+                $oldPath = public_path('students/' . $student->photo);
+                if (file_exists($oldPath)) unlink($oldPath);
+            }
+
+            // Save new image
+            $filename = time() . '_' . $req->photo->getClientOriginalName();
+            $req->photo->move(public_path('students'), $filename);
+            $data['photo'] = $filename;
         }
+
         $student->update($data);
+
         return new StudentResource($student);
     }
 
     public function destroy(Student $student)
     {
-        if ($student->photo) Storage::disk('public')->delete($student->photo);
+        if ($student->photo) {
+            $path = public_path('students/' . $student->photo);
+            if (file_exists($path)) unlink($path);
+        }
+
         $student->delete();
+
         return response()->noContent();
     }
 }
